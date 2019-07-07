c38609c99fc96910d1ddc9a65f33cf3b7404f007
muturi254 <petermuturi74@gmail.com>
2019-07-07 11:51:00 +0300

    Updates

---
+++ precache-manifest.eb56942fd5a0a8c6c5db488528370c56.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68fb793e363dcacb3a6e3eef1d783da5",
+    "url": "/react-cards/index.html"
+  },
+  {
+    "revision": "7e0a108f6bc3e3ed5a2c",
+    "url": "/react-cards/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c2a2185036c8d0c29d6f",
+    "url": "/react-cards/static/css/main.1c18cd79.chunk.css"
+  },
+  {
+    "revision": "7e0a108f6bc3e3ed5a2c",
+    "url": "/react-cards/static/js/2.73232a0f.chunk.js"
+  },
+  {
+    "revision": "c2a2185036c8d0c29d6f",
+    "url": "/react-cards/static/js/main.a56ad584.chunk.js"
+  },
+  {
+    "revision": "0812ece8d10d3f6d35d6",
+    "url": "/react-cards/static/js/runtime~main.a5beb306.js"
+  },
+  {
+    "revision": "a412a2898b3033731bac0900ef529eaa",
+    "url": "/react-cards/static/media/image1.a412a289.jpg"
+  },
+  {
+    "revision": "b3c2c6745b9421a13a3c4706b19223b3",
+    "url": "/react-cards/static/media/image2.b3c2c674.jpg"
+  },
+  {
+    "revision": "8f5ba5a455bb9ad5bedf6f62c6a03504",
+    "url": "/react-cards/static/media/image3.8f5ba5a4.jpg"
+  }
+]);
